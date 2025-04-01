62fa0270d7611537b53ee88c60fd20db47eb318b
Nick Agliano <nickagliano@gmail.com>
2025-03-31 22:16:33 -0400

    Remove test message

--- core/core/views.py
+++ core/core/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.contrib import messages
 
 def home(request):
-    messages.success(request, "Welcome! This is a test message!")
     return render(request, "home.html")
