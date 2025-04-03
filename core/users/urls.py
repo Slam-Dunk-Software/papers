f618d9ff03ca1c5fba40980c333bfd740679747d
Nick Agliano <nickagliano@gmail.com>
2025-04-03 09:15:34 -0400

    Try to update email template

--- core/users/urls.py
+++ core/users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("settings/", settings_view, name="settings"),
     # Password resets
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="auth/password_reset.html"), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="auth/password_reset.html", email_template_name='emails/password_reset_email.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="auth/password_reset_done.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="auth/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="auth/password_reset_complete.html"), name='password_reset_complete'),
