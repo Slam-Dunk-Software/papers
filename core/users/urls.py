4c7792ed879672354403a4d020918a615bbd6eea
Nick Agliano <nickagliano@gmail.com>
2025-04-03 09:20:16 -0400

    Fix, add HTML template

--- core/users/urls.py
+++ core/users/urls.py
@@ -9,7 +9,12 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("settings/", settings_view, name="settings"),
     # Password resets
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="auth/password_reset.html", email_template_name='emails/password_reset_email.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+      template_name="auth/password_reset.html",
+      subject_template_name='registration/password_reset_subject.txt',
+      email_template_name='emails/password_reset_email.txt',  # Text template
+      html_email_template_name='registration/password_reset_email.html'  # HTML template
+    ), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="auth/password_reset_done.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="auth/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="auth/password_reset_complete.html"), name='password_reset_complete'),
