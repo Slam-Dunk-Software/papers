083bfe1249c56a360a7aa51b51e6e2461ed08d82
Nick Agliano <nickagliano@gmail.com>
2025-04-06 19:33:51 -0400

    Fix type checking of django model

--- core/core/models.py
+++ core/core/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class WebhookLog(models.Model):
-    topic = models.CharField(max_length=255)
-    shop_domain = models.CharField(max_length=255, blank=True, null=True)
-    received_at = models.DateTimeField(default=timezone.now)
-    payload = models.JSONField()
+    topic = models.CharField(max_length=255)  # type: ignore
+    shop_domain = models.CharField(max_length=255, blank=True, null=True)  # type: ignore
+    received_at = models.DateTimeField(default=timezone.now)  # type: ignore
+    payload = models.JSONField()  # type: ignore
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.topic} @ {self.received_at.strftime('%Y-%m-%d %H:%M:%S')}"
