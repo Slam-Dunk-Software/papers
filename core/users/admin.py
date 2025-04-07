d9ffeb344a258a105004e75e3f39246b9da7e668
Nick Agliano <nickagliano@gmail.com>
2025-04-07 06:52:47 -0400

    Add readonly fields

--- core/users/admin.py
+++ core/users/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from core.models import Customer, CustomerAddress, WebhookLog
 
-
 @admin.register(WebhookLog)
 class WebhookLogAdmin(admin.ModelAdmin):
     list_display = ('topic', 'shop_domain', 'received_at')
     search_fields = ('topic', 'shop_domain')
     list_filter = ('topic', 'received_at')
+    readonly_fields = ('topic', 'shop_domain', 'received_at', 'payload')
+
 
 class CustomerAddressInline(admin.StackedInline):
     model = CustomerAddress
