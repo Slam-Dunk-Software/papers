f9b65de2d63331f9eb29397202613a1e868eb64a
Nick Agliano <nickagliano@gmail.com>
2025-04-07 06:45:14 -0400

    Add customers to Django admin

--- core/users/admin.py
+++ core/users/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from core.models import WebhookLog
+from core.models import Customer, CustomerAddress, WebhookLog
+
 
 @admin.register(WebhookLog)
 class WebhookLogAdmin(admin.ModelAdmin):
     list_display = ('topic', 'shop_domain', 'received_at')
     search_fields = ('topic', 'shop_domain')
     list_filter = ('topic', 'received_at')
+
+class CustomerAddressInline(admin.StackedInline):
+    model = CustomerAddress
+    extra = 0
+    readonly_fields = ('shopify_id',)  # optional
+    can_delete = False  # optional
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'shopify_id', 'first_name', 'last_name', 'email', 'state', 'created_at')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('state', 'currency', 'tax_exempt')
+    readonly_fields = ('created_at', 'updated_at', 'shopify_id')
+    inlines = [CustomerAddressInline]
+
